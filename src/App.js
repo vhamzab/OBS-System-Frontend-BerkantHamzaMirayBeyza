639c9758692fc5a091aa2a32612be67ad0650d6f
berkantbyr <147828059+berkantbyr@users.noreply.github.com>
2025-12-16 15:58:38 +0300

    Revert "güncelleme"
    This reverts commit 449d43cac52ec22ddeb53ac8c031d79957c38fd2.

--- src/App.js
+++
@@ -1,6 +0,0 @@
-// Bu dosya sadece klasör yapısı için eklendi.
-// Gerçek uygulama `App.jsx` içinde tanımlı.
-import App from './App.jsx';
-
-export default App;
-
--- src/index.js
+++
@@ -1,5 +0,0 @@
-// Bu dosya sadece klasör yapısına uymak için eklendi.
-// Vite projesinde gerçek giriş noktası `main.jsx` dosyasıdır.
-import './index.css';
-import './main.jsx';
-
